2e23b36a8e08b2308a0bdaec52fdfbb9dc5baa29
narisarit <rast777@naver.com>
2025-01-20 22:37:22 +0900

    add codes for tidy data deleted detailed arguments

--- tidy-data.R
+++ tidy-data.R
@@ -6,14 +6,41 @@
 # 1 row = several observations
 # one of the variables = stored in the header
 
-pivot_longer(df, cols = , names_to = , values_to = )
+pivot_longer()
 # transform col names to characters so we do 
 mutate(newcolname = as.numeric(newcolname))
-#or
-pivot_longer(df, cols = , names_to = "newcolname", values_to = , names_transform = list(newcolname=as.numeric))
 
-pivot_wider(df, names_from = , values_from = ) #sometimes useful intermediate step
 
-separate(data, col, into, sep = "[^[:alnum:]]+", remove = TRUE, convert = FALSE, extra = "warn", fill = "warn")
+pivot_wider() #sometimes useful intermediate step
 
-unite(data, col, ..., sep = "_", remove = TRUE, na.rm = FALSE)
+separate()
+
+unite()
+
+left_join()
+right_join()
+inner_join()
+full_join()
+semi_join()
+anti_join()
+
+bind_cols()
+bind_rows()
+
+intersect()
+union()
+setdiff()
+setequal()
+
+#web scraping
+library(rvest)
+html_nodes()
+html_node()
+html_table()
+html_text()
+html_form()
+set_values()
+submit_form()
+
+
+setNames()
