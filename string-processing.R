bc384676cbde87bc5449f1c956da1c52ccc781b0
narisarit <rast777@naver.com>
2025-01-21 23:52:31 +0900

    codes update about string processing, regular expression

--- string-processing.R
+++ string-processing.R
@@ -1,1 +1,23 @@
-string-processing
+# string-processing
+# stringr package is included in tidyverse package
+# the functions look like str_
+
+str_detect()
+str_replace_all()
+str_subset()
+str_view()
+parse_number()
+mutate_at()
+
+suppressWarnings()
+
+\\d
+\\s
+\\D
+[1-9]
+[a-z]
+[A-Z]
+[a-zA-Z]
+^
+$
+\\d{} 
