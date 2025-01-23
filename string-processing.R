6ba6380bf55a302a9f68740f5fc119219bc094ec
narisarit <rast777@naver.com>
2025-01-23 18:03:25 +0900

    code update for string processing

--- string-processing.R
+++ string-processing.R
@@ -3,21 +3,43 @@
 # the functions look like str_
 
 str_detect()
+str_replace()
 str_replace_all()
 str_subset()
 str_view()
+str_extract()
+str_match()
+str_trim()
+str_to_lower()
+str_split()
 parse_number()
 mutate_at()
+separate()
+extract() 
 
 suppressWarnings()
 
+map() #in purrr package
+map_chr()
+map_int()
+parse_guess()
+transpose()
+
+
+
 \\d
+\\d{} 
 \\s
 \\D
+\\i #i-th group
 [1-9]
 [a-z]
 [A-Z]
 [a-zA-Z]
 ^
 $
-\\d{} 
+
+* # 0 or more
++ # 1 or more
+? # 0 or 1
+
